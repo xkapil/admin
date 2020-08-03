a7d4b6439359d14ac83d2173ea32472e957c19a5
Kapil Malhotra <iamxkapil@gmail.com>
2020-08-03 17:53:37 +0530

    Create check_internet.py

---
+++ check_internet.py
@@ -0,0 +1,30 @@
+import logging
+import socket
+import time
+
+def internet(host="8.8.8.8", port=53, timeout=3):
+    """
+    Host: 8.8.8.8 (google-public-dns-a.google.com)
+    OpenPort: 53/tcp
+    Service: domain (DNS/TCP)
+    """
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except socket.error as ex:
+        print(ex)
+        return False
+
+# create logger with 'spam_application'
+logger = logging.getLogger('internet_app')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('conn.log')
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+while True:
+    logger.info(internet())
+    time.sleep(5)
